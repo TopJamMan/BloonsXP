b5bfa5f58baf6421fcd8a9a8a4020d7ce93165d2
TopJamMan <me@jaspermansfield.me>
2022-01-26 15:12:39 +0000

    Fixed broken run counter

--- gameend.py
+++ gameend.py
@@ -16,4 +16,4 @@ def game_end(running, successful_runs):
             print('Total successful runs:', successful_runs)
             pyautogui.click(x=950, y=900, interval=0.5)
             pyautogui.click(x=700, y=850, interval=3)
-            creatematch.create_match()
+            creatematch.create_match(successful_runs)
